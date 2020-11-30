166e00b2dcb731f265be48956608e096fb4822b7
20180217123 <1822993407@qq.com>
2020-11-30 20:48:23 +0800

    第一次提交

---
+++ localGitPro1/src/main/java/com/example/demo/LocalGitPro1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalGitPro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalGitPro1Application.class, args);
+	}
+
+}
